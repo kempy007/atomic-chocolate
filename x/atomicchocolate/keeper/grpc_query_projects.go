1f9977b59eeede5f37b414bf93c0e29674c7ded3
Martyn Kemp <mkemp@blockdaemon.com>
2022-10-14 07:58:13 +0100

    step4: Display posts

--- x/atomicchocolate/keeper/grpc_query_projects.go
+++ x/atomicchocolate/keeper/grpc_query_projects.go
@@ -3,7 +3,9 @@ package keeper
 import (
 	"context"
 
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/kempy007/atomic-chocolate/x/atomicchocolate/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -14,10 +16,26 @@ func (k Keeper) Projects(goCtx context.Context, req *types.QueryProjectsRequest)
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	var projects []*types.Project
+
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	store := ctx.KVStore(k.storeKey)
+	projectStore := prefix.NewStore(store, []byte(types.ProjectKey))
 
-	return &types.QueryProjectsResponse{}, nil
+	pageRes, err := query.Paginate(projectStore, req.Pagination, func(key []byte, value []byte) error {
+		var project types.Project
+		if err := k.cdc.Unmarshal(value, &project); err != nil {
+			return err
+		}
+
+		projects = append(projects, &project)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryProjectsResponse{Project: projects, Pagination: pageRes}, nil
 }
